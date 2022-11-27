f864bb8a1d38698c3a76ff3828ef1a0a9b0aa2e6
Lasse Lukkari <lasse.lukkari@gmail.com>
2022-11-27 10:51:00 +0200

    Updates

---
+++ precache-manifest.2009f2113c99b8e25378776fccd85298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53d215117cfd65dd44410648251ce9a",
+    "url": "/dxx-ui/index.html"
+  },
+  {
+    "revision": "afd080e7b999078804e9",
+    "url": "/dxx-ui/static/css/2.f0848b8c.chunk.css"
+  },
+  {
+    "revision": "180ce2a3eac9c1758c46",
+    "url": "/dxx-ui/static/css/main.6da01cc7.chunk.css"
+  },
+  {
+    "revision": "afd080e7b999078804e9",
+    "url": "/dxx-ui/static/js/2.7cef1da8.chunk.js"
+  },
+  {
+    "revision": "38c301faa7c92d3695011ef3c603ca06",
+    "url": "/dxx-ui/static/js/2.7cef1da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "180ce2a3eac9c1758c46",
+    "url": "/dxx-ui/static/js/main.5abb877d.chunk.js"
+  },
+  {
+    "revision": "07e40b4ce0f835acc971",
+    "url": "/dxx-ui/static/js/runtime-main.23c0a508.js"
+  }
+]);
